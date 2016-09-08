0b0c3607c1f8ad01bab8b0de86bc863b394fba30
Guido Barbaglia <guido.barbaglia@rea-group.com>
2016-09-08 11:56:55 +1000

    Trying to do AJAX request in the tests with poor results/

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -3,22 +3,21 @@ import fetch from 'isomorphic-fetch'
 export const RECEIVE_GENRES = 'RECEIVE_GENRES'
 export const REQUEST_GENRES = 'REQUEST_GENRES'
 
-function request_genres(json) {
+export const request_genres = () => {
   return {
     type: REQUEST_GENRES
   }
 }
 
-function fetch_genres () => {
+export const fetch_genres = () => {
   return dispatch => {
     dispatch(request_genres)
-    return fetch('http://api.nytimes.com/svc/books/v3/lists/names.json?api-key=sample-key')
-      .then(response => response.json())
-      .then(json => dispatch(receive_genres(json)))
+    return fetch('http://api.nytimes.com/svc/books/v3/lists/names.json?api-key=sample-key').then(response => response.json()).then(json => dispatch(receive_genres(json)))
   }
 }
 
 function receive_genres(json) {
+  console.log(json)
   return {
     type: RECEIVE_GENRES,
     genres: json.data.children.map(child => child.data)
---
+++ test/actions/index.js
@@ -0,0 +1,19 @@
+import expect from 'expect'
+import * as actions from '../../src/js/actions/index'
+
+describe('NYT Best Sellers actions', () => {
+
+  it('request_genres should create a REQUEST_GENRES action', () => {
+    expect(actions.request_genres()).toEqual({
+      type: 'REQUEST_GENRES'
+    })
+  })
+
+  it('fetch_genres should create a REQUEST_GENRES action', () => {
+    console.log(actions.fetch_genres())
+    // expect(actions.fetch_genres()).toEqual({
+    //   type: 'REQUEST_GENRES'
+    // })
+  })
+
+})
