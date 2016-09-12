149da26a8bbf1d6fb992a4561be40b07e3e926d8
Guido Barbaglia <guido.barbaglia@rea-group.com>
2016-09-12 16:41:29 +1000

    WIP.

--- test/actions/index.js
+++ test/actions/index.js
@@ -1,19 +1,31 @@
 import expect from 'expect'
-import * as actions from '../../src/js/actions/index'
+import sinon from 'sinon'
+import $ from 'jquery'
+import { fetch_genres } from '../../src/js/actions/index'
+
+sinon.xhr.supportsCORS = true
+sinon.useFakeXMLHttpRequest()
+
 
 describe('NYT Best Sellers actions', () => {
+    let dispatched, dispatcher
 
-  it('request_genres should create a REQUEST_GENRES action', () => {
-    expect(actions.request_genres()).toEqual({
-      type: 'REQUEST_GENRES'
+    beforeEach(done => {
+      global.$ = $
+      global.jQuery = $
+      global.server = sinon.fakeServer.create({ respondImmediately: true })
+      dispatched = []
+      dispatcher = (action) => { console.log(action); dispatched.push(action) }
+      global.server.respondWith(
+        'GET',
+        'http://api.nytimes.com/svc/books/v3/lists/names.json?api-key=sample-key',
+        [200, { 'Content-Type': 'application/json' }, '{"hallo": "World"}']
+      )
+      fetch_genres()(dispatcher).then(done, err => done(err))
     })
-  })
 
-  it('fetch_genres should create a REQUEST_GENRES action', () => {
-    // console.log(actions.fetch_genres())
-    // expect(actions.fetch_genres()).toEqual({
-    //   type: 'REQUEST_GENRES'
-    // })
-  })
-
-})
+    it('dispatches action FETCH_GENRES_SUCCESS with customer payload', () => {
+      expect(dispatched[1]).to.eql({ type: 'FETCH_GENRES_SUCCESS', customer })
+    })
+  }
+)
