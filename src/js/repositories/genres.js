3a47e3759f01a479be203ea54a01515e230b5693
Guido Barbaglia <guido.barbaglia@gmail.com>
2016-09-12 15:09:55 +1000

    Update genres.js

--- src/js/repositories/genres.js
+++ src/js/repositories/genres.js
@@ -4,3 +4,6 @@ export const getGenres = () => ajax({
   method: 'GET',
   url: 'http://api.nytimes.com/svc/books/v3/lists/names.json?api-key=sample-key'
 })
+/*
+add .then(responseBody => { return responseBody.results... })
+*/
